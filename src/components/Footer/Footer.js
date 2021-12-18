6aa04ce23558cc4ee60e3ee0990540b48434c032
Gonzalo Montañés <92688327+gonzalomontanes@users.noreply.github.com>
2021-12-18 18:34:57 +0100

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { AiFillGithub, AiFillBehanceSquare, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:contact@jsmastery.com">
+            g.montanes@hotmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Let's build something great together</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/gonzalomontanes">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/gonzalomontanes/?locale=en_US">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.behance.net/gonzalomontanes">
+            <AiFillBehanceSquare size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
---
+++ src/constants/constants.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: 'UX/UI',
+    description: "My profile in Behance consists of a selection of projects carried out in the field os UX/UI and product design, which has been my main activity thoughout my professional carreer.",
+      image: '/images/1.png',
+      tags: ['UX', 'UI', 'Product Management', 'AI', 'VUI', 'AR/VR'],
+    visit: 'https://www.behance.net/gonzalomontanes',
+    id: 0,
+  },
+  {
+    title: 'Programming',
+    description:"My Github profile hosts over 10 repositories of code which show how, parallel to my design activities, I continuously improve my skills in programming.",
+    image: '/images/2.png',
+    tags: ['HTML', 'CSS','Javascript', 'React', 'Chart', 'Node', 'D3',],
+    visit: 'https://github.com/gonzalomontanes',
+    id: 1,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey in the field of design.', },
+  { year: 2018, text: 'Introduced myself to UX design and started working on it full-time.', },
+  { year: 2019, text: 'Moved to China and solidified my experience in UX/UI at Shanghai Huiguo Tech.&Ed. and Suzhou Solonia Trading.', },
+  { year: 2020, text: 'Continued working remotely from Spain, after the outbreak of Covid-19.', },
+  { year: 2021, text: 'Obtained a position as UX designer in 5F Software, and moved to Germany, while furthering my knowledge in Dataviz, Voice UI and Product Management.', },
+];
